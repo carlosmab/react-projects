69ab66447e7ebc1183b57eca41b34597f6f1ce70
carlosmab <carlosm.araujobu@gmail.com>
2023-07-03 11:55:08 -0500

    Improved

--- amazon_books/src/components/books-list.js
+++ amazon_books/src/components/books-list.js
@@ -2,19 +2,14 @@ import { books } from "../data/books.js";
 import { EventExamples } from "./event-examples.js";
 import { Book } from "./book.js";
 
-const BooksList = () => {
+export const BooksList = () => {
 
   const someValue = "random value to display"; 
-  const getBook = (id) => {
-    const book = books.find(book => book.id === id);
-    console.log(book);
-  }
-
   return (
     <section className='book-list'>
       <EventExamples />
       {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />
+        return <Book {...book} key={book.id} />
       })}    
     </section>
   )
